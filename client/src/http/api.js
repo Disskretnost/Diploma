caffe19599ec12631875cb131c1325d257218c48
Disskretnost <disskretnost@mail.ru>
2025-03-06 11:58:13 +0300

    Получение домена

--- client/src/http/api.js
+++ client/src/http/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://138.124.93.138/api";
-export const SERVER_URL = "https://138.124.93.138";
+export const API_URL = "https://kucherenkoaleksanr.ru/api";
+export const SERVER_URL = "https://kucherenkoaleksanr.ru";
